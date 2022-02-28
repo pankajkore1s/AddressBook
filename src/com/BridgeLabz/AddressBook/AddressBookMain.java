2fa1d7d23b49492d86a69bf367751b66506fd617
pankaj kore <korep95@gmail.com>
2022-02-28 17:25:05 +0530

    [pankaj]added ability of creating new contact

--- src/com/BridgeLabz/AddressBook/AddressBookMain.java
+++ src/com/BridgeLabz/AddressBook/AddressBookMain.java
@@ -1,8 +1,22 @@
 package com.BridgeLabz.AddressBook;
-
+import java.util.*;
+import java.util.ArrayList;
 public class AddressBookMain {
+    public static Scanner scan=new Scanner(System.in);
     public static void main(String args[]){
-    System.out.println("!!!Welcome to Address Book Program!!!");
-    ContactPerson person = new ContactPerson();
-}
+        ContactDetailOperation contactDetail=new ContactDetailOperation();//object of ContactDetailOperation class is created
+        System.out.println("Address Book Menu!!");
+        System.out.println("1.Add contact");//choices for operation
+        System.out.print("enter choice:");
+        int choice=scan.nextInt();
+        switch (choice){
+            case 1:
+                contactDetail.addContact();
+                System.out.println("Contact Added Successfully!");
+                break;
+            default:
+                System.out.println("Please Enter Valid Choice:");
+                break;
+        }
+    }
 }
