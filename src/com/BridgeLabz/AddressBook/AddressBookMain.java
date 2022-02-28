376a454dea924707599615dd1baf91208b853b33
pankaj kore <korep95@gmail.com>
2022-02-28 17:17:33 +0530

    [pankaj]added ability of creating contact with diff parameteres

--- src/com/BridgeLabz/AddressBook/AddressBookMain.java
+++ src/com/BridgeLabz/AddressBook/AddressBookMain.java
@@ -3,6 +3,6 @@ package com.BridgeLabz.AddressBook;
 public class AddressBookMain {
     public static void main(String args[]){
     System.out.println("!!!Welcome to Address Book Program!!!");
-            ContactPerson person = new ContactPerson();
+    ContactPerson person = new ContactPerson();
 }
 }
